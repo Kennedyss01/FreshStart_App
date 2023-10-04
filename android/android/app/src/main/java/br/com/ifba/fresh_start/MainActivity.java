c3c550d8a6cbb0181abd9da6d20c098250437372
[Pedro Figueredo] <ph71542@gmail.com>
2023-10-04 09:39:28 -0300

    Adicionando a homescreen.

---
+++ android/android/app/src/main/java/br/com/ifba/fresh_start/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.ifba.fresh_start;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_start/homescreen.dart';
+
+void main() {
+  runApp(const FreshStart());
+}
+
+class FreshStart extends StatelessWidget {
+  const FreshStart({Key? key}) : super(key: key); // Corrigido super(key) para Key? key
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FreshStart',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
