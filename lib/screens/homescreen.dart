51d34e898c44b7a1e22b60513be9ff18d28b1b89
PedroHSilva10 <20211ireads0014@ifba.edu.br>
2023-10-07 17:05:36 -0300

    mudanças que Fiqueiredo fez na quarta

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: const Center(
+          child: Text(
+            'FreshStart',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ),
+        leading: InkWell(
+          onTap: () {
+            // Lead to profile
+          },
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage('user.png'),
+            ),
+          ),
+        ),
+
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildBottomIconButton(Icons.home, Colors.blue),
+            buildBottomIconButton(Icons.search, Colors.black45),
+            buildBottomIconButton(Icons.notifications, Colors.black45),
+            buildBottomIconButton(Icons.message, Colors.black45),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildBottomIconButton(IconData icon, Color color) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        color: color,
+      ),
+      onPressed: () {
+        buildBottomIconButton(icon, Colors.blue);
+
+      },
+    );
+  }
+}
